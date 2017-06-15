fae315af4523046863d663cb7e4bfc7ef77427a9
KampusHub <verbelesteban2@gmail.com>
2017-06-14 23:10:36 -0400

    Created and Implemented StreamLoader in both projects (Android and iOS)

---
+++ 1_IntroToCrossPlatDev/PCL/MyTunes.Droid/StreamLoader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MyTunes.Shared;
+
+namespace MyTunes
+{
+    public class StreamLoader : IStreamLoader
+    {
+        private readonly Context _context;
+        
+        public StreamLoader(Context context)
+        {
+            // Android assets are loaded through an Android Context so pass in a Context to the constructor
+            _context = context;
+        }
+        
+        public Stream GetStreamForFilename(string fileName)
+        {
+            return _context.Assets.Open(fileName);
+        }
+    }
+}
---
+++ 1_IntroToCrossPlatDev/PCL/MyTunes.iOS/StreamLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using MyTunes.Shared;
+using System.IO;
+
+namespace MyTunes
+{
+    public class StreamLoader : IStreamLoader
+    {
+        public Stream GetStreamForFilename(string fileName)
+        {
+            return File.OpenRead(fileName);
+        }
+    }
+}
