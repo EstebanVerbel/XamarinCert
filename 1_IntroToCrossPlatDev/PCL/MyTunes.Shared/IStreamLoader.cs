65bec1aca5f00d364d74cca6093f33e71864283d
KampusHub <verbelesteban2@gmail.com>
2017-06-14 22:58:49 -0400

    Created IStreamLoader interface

---
+++ 1_IntroToCrossPlatDev/PCL/MyTunes.Shared/IStreamLoader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTunes.Shared
+{
+    public interface IStreamLoader
+    {
+
+        Stream GetStreamForFilename(string fileName);
+
+    }
+}
