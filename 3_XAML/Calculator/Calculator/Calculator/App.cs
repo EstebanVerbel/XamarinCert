f50846af570de422c7d80db6007b6ed1f2b47c53
KampusHub <verbelesteban2@gmail.com>
2017-06-26 22:49:44 -0400

    Added XamlCompilation

--- 3_XAML/Calculator/Calculator/Calculator/App.cs
+++ 3_XAML/Calculator/Calculator/Calculator/App.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 
 namespace Calculator
 {
-	public class App : Application
+    public class App : Application
 	{
 		public App()
 		{
