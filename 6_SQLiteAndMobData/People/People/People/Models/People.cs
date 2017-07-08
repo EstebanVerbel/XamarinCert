9b8c9372af0f74b5cfc1e1c09fb9cb23160a6bf2
KampusHub <verbelesteban2@gmail.com>
2017-07-08 15:21:10 -0400

    Added SQLite Data Anotations to the People class

--- 6_SQLiteAndMobData/People/People/People/Models/People.cs
+++ 6_SQLiteAndMobData/People/People/People/Models/People.cs
@@ -1,10 +1,14 @@
-﻿namespace People.Models
+﻿using SQLite;
+
+namespace People.Models
 {
+    [Table("people")]
     public class People
     {
-
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
+        [MaxLength(250), Unique]
         public string Name { get; set; }
 
     }
