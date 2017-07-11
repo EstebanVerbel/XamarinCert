b02ff901a49f0095aae1412b70191cd6e3dfdbb6
KampusHub <verbelesteban2@gmail.com>
2017-07-10 23:09:10 -0400

    Added handler to Set type of connection when it changes

--- 7_ConsRESTWebServ/NetStatus/NetStatus/NetStatus/NetworkViewPage.xaml.cs
+++ 7_ConsRESTWebServ/NetStatus/NetStatus/NetStatus/NetworkViewPage.xaml.cs
@@ -18,11 +18,36 @@ namespace NetStatus
 
         protected override void OnAppearing()
         {
+            base.OnAppearing();
+
+            SetConnectionTypeLabelText();
+
+            CrossConnectivity.Current.ConnectivityChanged += UpdateNetworkInfo;
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            // remove handler on dissapearing to avoid memory leaks since the event is static
+            if (CrossConnectivity.Current != null)
+                CrossConnectivity.Current.ConnectivityChanged -= UpdateNetworkInfo;
+        }
+
+        private void SetConnectionTypeLabelText()
+        {
             string connectionType = CrossConnectivity.Current.ConnectionTypes.First().ToString();
 
             // set the label's text
             ConnectionDetails.Text = connectionType;
         }
-        
+
+
+        private void UpdateNetworkInfo(object sender, Plugin.Connectivity.Abstractions.ConnectivityChangedEventArgs e)
+        {
+
+            if (CrossConnectivity.Current != null && CrossConnectivity.Current.ConnectionTypes != null)
+                SetConnectionTypeLabelText();
+
+        }
     }
 }
