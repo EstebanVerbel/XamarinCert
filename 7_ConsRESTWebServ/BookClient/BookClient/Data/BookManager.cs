32cfc1247741d0597c7e8d512ee12fddf2e08c26
KampusHub <verbelesteban2@gmail.com>
2017-07-12 20:12:56 -0400

    Added method to get http client  and token (authorization key) if first call

--- 7_ConsRESTWebServ/BookClient/BookClient/Data/BookManager.cs
+++ 7_ConsRESTWebServ/BookClient/BookClient/Data/BookManager.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -7,6 +9,15 @@ namespace BookClient.Data
 {
     public class BookManager
     {
+        #region -- Members --
+
+        private const string Url = "http://xam150.azurewebsites.net/api/books/";
+        private string _authorizationKey;
+
+        #endregion
+
+        #region -- Public Methods --
+
         public Task<IEnumerable<Book>> GetAll()
         {
             // TODO: use GET to retrieve books
@@ -30,6 +41,32 @@ namespace BookClient.Data
             // TODO: use DELETE to delete a book
             throw new NotImplementedException();
         }
+
+        #endregion
+
+        #region -- Private Methods --
+
+        private async Task<HttpClient> GetClient()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            // get authorization key only if it is the first time we're making a call to the web service
+            if (string.IsNullOrEmpty(_authorizationKey))
+            {
+                _authorizationKey = await httpClient.GetStringAsync($"{Url}login");
+                _authorizationKey = JsonConvert.DeserializeObject<string>(_authorizationKey);
+            }
+
+            // adds authorization key (token) to httpclient 
+            httpClient.DefaultRequestHeaders.Add("Authorization", _authorizationKey);
+            // this header is added so that all of the responses are returned as json
+            httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            return httpClient;
+        }
+
+        #endregion
+
     }
 }
 
