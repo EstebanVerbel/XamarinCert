fbd0cc61b5ca68998b22bb65b8ad38070d1d94de
KampusHub <verbelesteban2@gmail.com>
2017-07-19 22:23:12 -0400

    Added EmbeddedImageConverter to be able to bind the Image in the UI
    It implements the IValueConverterInterface

---
+++ 8_DataBinding/Lab/Part_1/Start/FunFlacts/Converters/EmbeddedImageConverter.cs
@@ -0,0 +1,37 @@
+﻿using FlagData;
+using FunFlacts.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FunFlacts.Converters
+{
+    public class EmbeddedImageConverter : IValueConverter
+    {
+        /// <summary>
+        /// Optional type located in the assembly we want to get the resource from - if not supplied, 
+        /// the API assumes the resource is located in this assembly.
+        /// </summary>
+        public Type ResolvingAssemblyType { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string imageUrl = (value ?? "").ToString();
+
+            if (string.IsNullOrEmpty(imageUrl))
+                return null;
+            
+            return ImageSource.FromResource(imageUrl, ResolvingAssemblyType?.GetTypeInfo().Assembly);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
