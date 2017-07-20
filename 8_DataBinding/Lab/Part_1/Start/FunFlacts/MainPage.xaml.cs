c405599ade4976432abc7e632e3181235bccc1e4
KampusHub <verbelesteban2@gmail.com>
2017-07-19 21:32:44 -0400

    Adding one year every time search button is clicked to Flags DateAdopted

--- 8_DataBinding/Lab/Part_1/Start/FunFlacts/MainPage.xaml.cs
+++ 8_DataBinding/Lab/Part_1/Start/FunFlacts/MainPage.xaml.cs
@@ -42,6 +42,8 @@ namespace FunFlacts
 
 		private async void OnShow(object sender, EventArgs e)
 		{
+            CurrentFlag.DateAdopted = CurrentFlag.DateAdopted.AddYears(1);
+
             await DisplayAlert(CurrentFlag.Country,
 				$"{CurrentFlag.DateAdopted:D} - {CurrentFlag.IncludesShield}: {CurrentFlag.MoreInformationUrl}", 
 				"OK");
