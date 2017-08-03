a0baf1f3e07d459a603bf134b3de18ab21839a5e
KampusHub <verbelesteban2@gmail.com>
2017-08-03 03:30:23 -0400

    Added ListView and is now bound to Repo

--- 10_ListViews/Lab Materials/Part 01 Resources/Start/FunFlacts/AllFlags.xaml.cs
+++ 10_ListViews/Lab Materials/Part 01 Resources/Start/FunFlacts/AllFlags.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlagData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,8 @@ namespace FunFlacts
         public AllFlags()
         {
             InitializeComponent();
+
+            flags.ItemsSource = DependencyService.Get<FunFlactsViewModel>().Flags;
         }
     }
 }
