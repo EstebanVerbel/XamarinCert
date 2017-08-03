ee022c0eaf0f5a4b507f8fe0f7ebc23ec7062de8
KampusHub <verbelesteban2@gmail.com>
2017-08-03 03:57:44 -0400

    Added Binding directly on xaml to Flags collection and hande selected item that way

--- 10_ListViews/Lab Materials/Part 01 Resources/Start/FunFlacts/AllFlags.xaml.cs
+++ 10_ListViews/Lab Materials/Part 01 Resources/Start/FunFlacts/AllFlags.xaml.cs
@@ -15,15 +15,20 @@ namespace FunFlacts
         {
             InitializeComponent();
 
-            flags.ItemsSource = DependencyService.Get<FunFlactsViewModel>().Flags;
+            //flags.ItemsSource = DependencyService.Get<FunFlactsViewModel>().Flags;
 
-            flags.ItemTapped += OnItemTapped;
+            BindingContext = DependencyService.Get<FunFlactsViewModel>();
+
+            // flags.ItemTapped += OnItemTapped;
         }
 
         async void OnItemTapped(object sender, ItemTappedEventArgs e)
         {
-            DependencyService.Get<FunFlactsViewModel>().CurrentFlag = (Flag)e.Item;
+          //  DependencyService.Get<FunFlactsViewModel>().CurrentFlag = (Flag)e.Item;
+
+
             await this.Navigation.PushAsync(new FlagDetailsPage());
+            flags.SelectedItem = null;
         }
 
 
