56ff1b83a0dd49f6d516af84c9a69928a3c436d1
KampusHub <verbelesteban2@gmail.com>
2017-08-12 00:29:20 -0400

    Created IQuoteLoader interface

---
+++ 11_AdvancedCrossPlat/GreatQuotes/GreatQuotes.Data/GreatQuotes.Data/IQuoteLoader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace GreatQuotes.Data
+{
+    public interface IQuoteLoader
+    {
+        IEnumerable<GreatQuote> Load();
+
+        void Save(IEnumerable<GreatQuote> quotes);
+    }
+}
