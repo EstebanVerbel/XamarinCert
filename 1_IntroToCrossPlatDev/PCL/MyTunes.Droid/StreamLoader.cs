4f15776b1a056095789e82655cf25e8a4decf1f3
KampusHub <verbelesteban2@gmail.com>
2017-08-12 00:05:13 -0400

    Revert "Advances Cross Platform start material"
    This reverts commit 834a874da80c80cb69a0e9475ec862829710a2e8.

--- 1_IntroToCrossPlatDev/PCL/MyTunes.Droid/StreamLoader.cs
+++ 1_IntroToCrossPlatDev/PCL/MyTunes.Droid/StreamLoader.cs
@@ -1,6 +1,16 @@
-﻿using Android.Content;
-using MyTunes.Shared;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MyTunes.Shared;
 
 namespace MyTunes
 {
