35a8012fb085f7b3c389d3a4acdf609b8bd1cff4
KampusHub <verbelesteban2@gmail.com>
2017-08-25 23:02:24 -0400

    Added calculation for tip and updates of views on ui to display it

--- 12_IntroToAndroid/TipCalculator/TipCalculator/MainActivity.cs
+++ 12_IntroToAndroid/TipCalculator/TipCalculator/MainActivity.cs
@@ -11,6 +11,10 @@ namespace TipCalculator
     [Activity(Label = "TipCalculator", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private EditText _inputBill;
+        private Button _calculateButton;
+        private TextView _outputTip;
+        private TextView _outputTotal;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -18,7 +22,28 @@ namespace TipCalculator
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-            
+
+            // find views on UI by id
+            _inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            _calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            _outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            _outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+
+            // subscribe handdler to click event
+            _calculateButton.Click += calculateButton_Click;
+
+
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            float inputBill = float.Parse(_inputBill.Text);
+
+            float tip = inputBill * 0.15f;
+            float total = inputBill + tip;
+
+            _outputTip.Text = tip.ToString();
+            _outputTotal.Text = total.ToString();
         }
     }
 }
